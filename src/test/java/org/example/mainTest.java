1be79a7b4a858f335d477b1f133438da0372d1c9
jubitjohn1 <jubit.john@prevalent.ai>
2023-08-08 15:06:33 +0530

    Changes

--- src/test/java/org/example/mainTest.java
+++ src/test/java/org/example/mainTest.java
@@ -2,47 +2,42 @@ package org.example;
 import java.io.IOException;
 import java.sql.Connection;
 import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
 import utils.databaseUtil;
-import org.example.CSVDataReader;
-
 import org.junit.Test;
 import static org.junit.Assert.*;
 
 public class mainTest {
 
     @Test
-    public void testMainMethod() throws SQLException, ClassNotFoundException, IOException {
-//        // Set up mock objects and test data
-//        DataReader mockDataReader = mock(DataReader.class);
-//        Connection mockConnection = mock(Connection.class);
-//        PreparedStatement mockStatement = mock(PreparedStatement.class);
-//
-//        // Create an instance of Main with mock objects
-//        Main main = new Main(mockDataReader, mockConnection, mockStatement);
-//
-//        // Set up test data
-//        List<String[]> testData = new ArrayList<>();
-//        testData.add(new String[]{"1", "Product 1", "Vendor 1", "10", "100.0", "5.0", "10.0", "Region 1", "Category 1", "0.1"});
-//        when(mockDataReader.readData(anyString())).thenReturn(testData);
-//
-//        // Verify interactions and assertions
-//        verify(mockConnection).prepareStatement(anyString());
-//        verify(mockStatement, times(1)).executeBatch();
-//
-//        // Additional assertions can be added based on your requirements
-
+    public void testConnection() throws SQLException,ClassNotFoundException,IOException {
 
         Connection Result = databaseUtil.getConnection();
 
         assertNotNull(Result);
+    }
+
+    @Test
+    public void testCsvMethod() throws SQLException, ClassNotFoundException, IOException {
 
         CSVDataReader csvDataReader = new CSVDataReader();
 
-        csvDataReader.readData()
+        List<String[]> res= csvDataReader.readData("C:\\Users\\jubit.john\\IdeaProjects\\maven_project\\src\\test\\resources\\testDemo.csv");
+        List<String[]> expectedData = new ArrayList<>();
+        String[] dataRow = {
+                "1", "Eldon Base for stackable storage shelf", "Muhammed MacIntyre", "3", "-213.25", "38.94", "35", "Nunavut", "Storage & Organization", "0.8"
+        };
+        expectedData.add(dataRow);
 
+        assertEquals(expectedData.size(), res.size());
+        for (int i = 0; i < expectedData.size(); i++) {
+            assertArrayEquals(expectedData.get(i), res.get(i));
+        }
 
 
         };
 
 
-    }
+};
+
