bd29a0b52daabceb8dd2f434162818a999666235
jubitjohn1 <jubit.john@prevalent.ai>
2023-08-09 11:41:55 +0530

    git ignore updated

--- src/main/java/org/example/JSONDataReader.java
+++ src/main/java/org/example/JSONDataReader.java
@@ -1,10 +1,8 @@
 package org.example;
-
 import org.json.simple.JSONArray;
 import org.json.simple.JSONObject;
 import org.json.simple.parser.JSONParser;
 import org.json.simple.parser.ParseException;
-
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
